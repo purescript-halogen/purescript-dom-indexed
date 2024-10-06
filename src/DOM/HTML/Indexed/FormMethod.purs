8e0c535d970171d1d921875cadfa2fde6f96dd03
Konstantin K <Hi-Angel@yandex.ru>
2024-10-06 20:20:23 +0300

    Implement "dialog" form method value (#34)
    See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form#method

--- src/DOM/HTML/Indexed/FormMethod.purs
+++ src/DOM/HTML/Indexed/FormMethod.purs
@@ -5,6 +5,7 @@ import Prelude
 data FormMethod
   = POST
   | GET
+  | Dialog
 
 derive instance eqFormMethod :: Eq FormMethod
 derive instance ordFormMethod :: Ord FormMethod
@@ -13,3 +14,4 @@ renderFormMethod :: FormMethod -> String
 renderFormMethod = case _ of
   POST -> "post"
   GET -> "get"
+  Dialog -> "dialog"
